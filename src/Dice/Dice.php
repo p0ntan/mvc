cb5f3e0d3f5d98bab415a0b8cf8306bfc4a0ce7d
p0ntan <pontus.akerberg@hotmail.com>
2023-04-06 18:49:59 +0200

    Add files for dice-game pig

---
+++ src/Dice/Dice.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Dice;
+
+/**
+ * Dice class for dicegame
+ */
+class Dice
+{
+    protected $value;
+
+    public function __construct()
+    {
+        $this->value = null;
+    }
+
+    public function roll(): int
+    {
+        $this->value = random_int(1, 6);
+        return $this->value;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function getAsString(): string
+    {
+        return "[{$this->value}]";
+    }
+}
