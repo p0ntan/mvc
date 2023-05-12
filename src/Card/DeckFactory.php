807c14256f55f9787796f57b1374e83d588b380e
p0ntan <pontus.akerberg@hotmail.com>
2023-05-12 17:30:42 +0200

    Updated code to get less complexity

--- src/Card/DeckFactory.php
+++ src/Card/DeckFactory.php
@@ -10,25 +10,39 @@ class DeckFactory
 {
     public function createDeck(DeckOfCards $cardDeck, string $typeOfDeck): DeckOfCards
     {
+        if ($typeOfDeck == "Card") {
+            $cardDeck = $this->card($cardDeck);
+        } elseif ($typeOfDeck == "CardGraphic") {
+            $cardDeck = $this->cardGraphic($cardDeck);
+        }
+
+        $cardDeck->shuffleDeck();
+        return $cardDeck;
+    }
+
+    private function card(DeckOfCards $cardDeck) {
         $allSuits = Card::SUITS;
         $allValues = Card::VALUES;
 
-        if ($typeOfDeck == "Card") {
-            foreach ($allSuits as $suit) {
-                foreach ($allValues as $value) {
-                    $newCard = new Card($suit, $value);
-                    $cardDeck->addCard($newCard);
-                }
-            }
-        } elseif ($typeOfDeck == "CardGraphic") {
-            foreach ($allSuits as $suit) {
-                foreach ($allValues as $value) {
-                    $newCard = new CardGraphic($suit, $value);
-                    $cardDeck->addCard($newCard);
-                }
+        foreach ($allSuits as $suit) {
+            foreach ($allValues as $value) {
+                $newCard = new Card($suit, $value);
+                $cardDeck->addCard($newCard);
             }
         }
-        $cardDeck->shuffleDeck();
+        return $cardDeck;
+    }
+
+    private function cardGraphic(DeckOfCards $cardDeck) {
+        $allSuits = Card::SUITS;
+        $allValues = Card::VALUES;
+
+        foreach ($allSuits as $suit) {
+            foreach ($allValues as $value) {
+                $newCard = new CardGraphic($suit, $value);
+                $cardDeck->addCard($newCard);
+            }
+        }
         return $cardDeck;
     }
 }
